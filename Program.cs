ec19fa3da857f2e2aecd34294dda9fd9b61fb47c
Alberto A <Alberto.Arpini@fmc-ag.com>
2020-07-18 19:31:02 +0200

    Extension Method CountBy

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Linq
 {
@@ -6,7 +8,39 @@ namespace Linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var c = "Dog,Cat,Rabbit,Dog,Dog,Lizard,Cat,Cat,Dog,Rabbit,Guinea,Pig,Dog"
+                .Split(",")
+                .CountBy(x => (x == "Dog" || x == "Cat") ? x : "other");
+
+            foreach (var x in c)
+            {
+                Console.WriteLine($"{x.Key} {x.Value}");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+
+    static class MyLinqExtensions
+    {
+        public static IEnumerable<KeyValuePair<TKey, int>> CountBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector)
+        {
+            var counts = new Dictionary<TKey, int>();
+            foreach (var item in source)
+            {
+                var key = selector(item);
+                if (!counts.ContainsKey(key))
+                {
+                    counts[key] = 1;
+                }
+                else
+                {
+                    counts[key]++;
+                }
+            }
+
+            return counts;
         }
     }
 }
