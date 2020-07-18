f799ca8f4c3fca2d96da2f6b244594b575d186f9
Alberto A <Alberto.Arpini@fmc-ag.com>
2020-07-18 22:19:06 +0200

    MoreLinq Pairwise example

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using static MoreLinq.Extensions.PairwiseExtension;
 
 namespace Linq
 {
@@ -8,17 +9,40 @@ namespace Linq
     {
         static void Main(string[] args)
         {
-            var c = "Dog,Cat,Rabbit,Dog,Dog,Lizard,Cat,Cat,Dog,Rabbit,Guinea,Pig,Dog"
+            // CustomExtensionMethod();
+
+            MoreLinq();
+
+            Console.ReadLine();
+
+        }
+
+        private static void MoreLinq()
+        {
+            var splitTimes = "00:45,01:32,02:15,03:01,03:44,04:31,05:19,06:01,06:47,07:35";
+
+            var pairWiseDifferences = splitTimes
                 .Split(",")
-                .CountBy(x => (x == "Dog" || x == "Cat") ? x : "other");
+                .Select(x => TimeSpan.Parse("00:" + x))
+                .Prepend(TimeSpan.Zero)
+                .Pairwise((a, b) => b - a);
+
+            foreach (var item in pairWiseDifferences)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void CustomExtensionMethod()
+        {
+            var c = "Dog,Cat,Rabbit,Dog,Dog,Lizard,Cat,Cat,Dog,Rabbit,Guinea,Pig,Dog"
+                            .Split(",")
+                            .CountBy(x => (x == "Dog" || x == "Cat") ? x : "other");
 
             foreach (var x in c)
             {
                 Console.WriteLine($"{x.Key} {x.Value}");
             }
-
-            Console.ReadLine();
-
         }
     }
 
